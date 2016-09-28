15bc2f8ecf0ab5055cf349a1fe35a535c5d127c9
pan.min <pan.min@tcl.com>
2016-09-28 20:01:13 +0800

    add redis business module and test example

---
+++ tworking-biz/src/test/java/com/tcl/tworking/biz/service/common/AbstractBaseServiceTest.java
@@ -0,0 +1,11 @@
+package com.tcl.tworking.biz.service.common;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+/**
+ * Created by panmin on 9/28/16.
+ */
+@ContextConfiguration(locations = {"classpath:applicationContext-biz-test.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public abstract class AbstractBaseServiceTest {}
